dee1cdfc6744617deca309c08236a3158b619a48
Souvik Roy <souvikroy@Souviks-MacBook-Air.local>
2026-06-22 15:00:23 +0530

    initialize project structure

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='w-full h-screen bg-zinc-500'>
+
+      </div>
+    </>
+  )
+}
+
+export default App
