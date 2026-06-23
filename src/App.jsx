d0f21321025214465f0a6659f71f28c7aaab3aac
Kxrishx03 <2001karishmamandal@gmail.com>
2026-06-23 15:36:14 +0530

    All files added for routing & initial project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AppLayout from "./components/layout/AppLayout";
+import AppLayout from "./components/layouts/AppLayout";
 import HomePage from "./pages/HomePage";
 import GalleryPage from "./pages/GalleryPage";
-import GalleryPlaygroundPage from "./pages/GalleryPlaygroundPage";
 import BuilderPage from "./pages/BuilderPage";
 
 const router = createBrowserRouter([
@@ -12,7 +11,6 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage /> },
       { path: "gallery", element: <GalleryPage /> },
-      { path: "gallery/:type/:variant", element: <GalleryPlaygroundPage /> },
       { path: "builder", element: <BuilderPage /> },
     ],
   },
--- src/components/layouts/AppLayout/AppLayout.jsx
+++ src/components/layouts/AppLayout/AppLayout.jsx
@@ -3,8 +3,9 @@ import AppHeader from "../AppHeader";
 
 export default function AppLayout() {
   return (
-    <div className="flex min-h-screen flex-col bg-[#0a0a0f]">
+    <div className="flex min-h-screen flex-col bg-gray-50">
       <AppHeader />
+
       <main className="flex-1 pt-14">
         <Outlet />
       </main>
